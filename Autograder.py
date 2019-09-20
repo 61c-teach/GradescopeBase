33d898e14e755d6e2e89f2421bbc1a2194fd45ba
Stephan Kaminsky <thaumicmekanism@gmail.com>
2019-09-20 11:55:45 -0700

    Added comment about the root dir assumption

--- Autograder.py
+++ Autograder.py
@@ -133,5 +133,8 @@ class Autograder:
 
     @staticmethod
     def root_dir() -> str:
+        """
+        This function assumes the root directory is the one right above the GradescopeBase folder.
+        """
         dirname = os.path.dirname
         return dirname(dirname(os.path.realpath(__file__)))
