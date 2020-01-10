c1e1f7e64f7b2a474a7808adde542cca5af81826
Stephan Kaminsky <thaumicmekanism@gmail.com>
2020-01-09 21:51:27 -0800

    Updated error message

--- Autograder.py
+++ Autograder.py
@@ -185,7 +185,7 @@ class Autograder:
         else:
             if "tests" not in results or len(results["tests"]) == 0 or not any(["score" in t for t in results["tests"]]):
                 results["score"] = 0
-                self.print("This autograder does not have any tests which give points!")
+                self.print("This autograder does not set the main score or have any tests which give points!")
         if self.output is not None:
             results["output"] = self.output
         if self.visibility is not None:
